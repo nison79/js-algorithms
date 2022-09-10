74de780bb7e88ada4c64ca5c91f079640b4ff3e6
George Nikoglou <georgenison@gmail.com>
2022-09-10 20:42:35 +0300

    first alg

---
+++ problem-solving-approach.js
@@ -0,0 +1,10 @@
+// ? write a function which takes in a  string and 
+//  ? returns counts of each character in the string 
+const charCount = (str) => {
+    //?  do something
+   // ?loop over the string, for each character...
+   // ? if the char is a number/letter AND is a key in object, add one to count
+   // ? if the char is a number/letter AND not in object, add it to object and set value to 1
+   // ? if the char is something else (space, period, etc.) don't do anything
+   // ? return object at end
+}
